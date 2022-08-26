5eaa88bf4b646f786f858dd013eecb0a11d35d82
peeterliik <liikpeeter@gmail.com>
2022-08-26 12:14:53 +0300

    refactoring

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.droidSafeArea}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.title}>
+          <Text>title</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  droidSafeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "#33A8FF",
+  },
+  title: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#33FFBB",
   },
 });
