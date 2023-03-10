de6bd0a1f95b357b873fbad12fb525f9ddb7d7fd
peeterliik <liikpeeter@gmail.com>
2023-03-10 13:10:16 +0200

    10.03

---
+++ src/features/map/components/map-callout.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { CompactRestaurant } from "../../../components/restuaurants/compact-restaurant.component";
+
+export const MapCallout = ({ restaurant }) => (
+  <>
+    <CompactRestaurant restaurant={restaurant}></CompactRestaurant>
+  </>
+);
